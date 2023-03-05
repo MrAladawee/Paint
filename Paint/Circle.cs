b502971447e93bf2cb78f8b92a6cfb0b2a16cb6b
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2023-03-05 23:08:13 +0300

    Добавлено изменение ширины фигур

--- Paint/Circle.cs
+++ Paint/Circle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +13,7 @@ namespace Paint
         {
             try
             {
-                var p = new Pen(Color);
+                var p = new Pen(Color, penSize);
                 g.DrawEllipse(p, Rectangle);
             }
             catch
